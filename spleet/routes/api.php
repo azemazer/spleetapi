e195a693edbd9d0ea7862ad5b6ab0f7d7fb4bf55
Boismain Florian <boismain.florian@gmail.com>
2024-11-08 11:51:00 +0100

    update

--- spleet/routes/api.php
+++ spleet/routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+    return response()->json(['message' => 'Déconnexion réussie']);
+});
+
